575834a05f1874af9cdb0e7ebe54bd40c3dd557b
Tianrui Li <tealeaf@icari.io>
2022-01-25 18:32:45 -0500

    Adds layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  blogEntries: any[] = [
+    { 
+      title: "Cute Panda",
+      author: "Panda Man",
+      imgSrc: "assets/panda.jpg",
+      text: "Pandas are great"
+    },
+    { 
+      title: "Cute Panda 2",
+      author: "Panda Man Jr.",
+      imgSrc: "assets/panda.jpg",
+      text: "Pandas are great again!"
+    }
+  ]
 
   constructor() {}
 
